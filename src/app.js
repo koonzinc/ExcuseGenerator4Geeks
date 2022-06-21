4ef62413d25cfacf95eabcb30d68bc4b178d62c2
koonzinc <williamgeorgekoonz@gmail.com>
2022-06-21 17:20:36 +0000

    update

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
