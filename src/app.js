b065f97c5e93c5e7e38188753e6c957a74784d8f
koonzinc <105440252+koonzinc@users.noreply.github.com>
2022-06-21 13:22:16 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+
+  let pronoun = ['The', 'A'];
+  let subject = ['cat', 'bear', 'bull', 'lion'];
+  let action = ['burned my', 'buried my', 'ate my', 'ripped my'];
+  let object = ['homework', 'shoes', 'car', 'shirt'];
+  let location = ['on the lawn', 'in the pool', 'in my house', 'in the bathroom'];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let objectIndex = Math.floor(Math.random() * object.length);
+  let locationIndex = Math.floor(Math.random() * location.length);
+
+  return pronoun[pronounIndex] + ' ' + subject[subjectIndex] + ' ' + action[actionIndex] + ' ' + object[objectIndex] + ' ' + location[locationIndex];
 };
